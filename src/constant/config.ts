2668afd38b4df2b9dcce7bbdfac013adea460d25
hangliu <liuhang20020917@gmail.com>
2025-10-03 14:58:55 -0400

    UPDATE:

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   title: 'Ego-Vision World Model for Humanoid Contact Planning',
   description: '',
-  url: 'https://66lau.github.io/ego-vc/',
+  url: 'https://ego-vcp.github.io/',
 };
